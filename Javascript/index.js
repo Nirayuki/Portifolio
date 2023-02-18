cd35495ffabc1b09716607e7142525cf768e076f
Nirayuki <aronkerk8@gmail.com>
2023-02-18 00:12:27 -0300

    Update mobile responsive nav

---
+++ Javascript/index.js
@@ -0,0 +1,44 @@
+class Mobile{
+    constructor(mobile, navListMobile, navLinks){
+        this.mobile = document.querySelector(mobile);
+        this.navListMobile = document.querySelector(navListMobile);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        })
+    }
+
+    handleClick(){
+        console.log(this);
+        this.navListMobile.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobile.addEventListener("click", this.handleClick);
+
+    }
+
+    init(){
+        if(this.mobile){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+
+const mobile = new Mobile(
+    ".mobile",
+    ".nav-list-mobile",
+    ".nav-list-mobile li"
+)
+
+mobile.init();
